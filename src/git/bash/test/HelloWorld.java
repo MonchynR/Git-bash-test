7719af1be8bfccc8dac57f7091a93423e3dc357d
Roman Monchyn <monchynroman@gmail.com>
2016-08-06 16:31:41 +0300

    Have HelloWorld java class with 2 system.out

---
+++ src/git/bash/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package git.bash.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println("Hello, Lviv!");
+	}
+}
